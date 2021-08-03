bea41ede367097a9f5a583848cfd9eaff368cb0c
Rockyfelle <rockyfelle@live.se>
2021-08-03 11:41:45 +0200

    Vehicle Page Update

--- resources/js/components/data/vehicles/view.js
+++ resources/js/components/data/vehicles/view.js
@@ -14,6 +14,9 @@ import {
 	Select,
 } from 'semantic-ui-react';
 import ReactFrappeChart from 'react-frappe-charts';
+import { GiCarWheel } from 'react-icons/gi';
+import { ImCross } from 'react-icons/Im';
+import { HiArrowLeft } from 'react-icons/Hi';
 
 
 const mileage = {
@@ -103,12 +106,73 @@ const fuelArray = [
 	{ date: '19-10-03', year: '2019', month: '10', day: '03', cost: '555' },
 ]
 
+const carData = {
+	wheels: {
+		winter: {
+			type: 'Allround',
+			amount: 2,
+		},
+		summer: {
+			type: 'Summer',
+			amount: 4,
+		},
+		current: 'summer',
+	},
+	insuranceCost: '6601',
+	plate: 'LKJ689',
+	model: 'Toyota Aygo 1.0 2016',
+	color: 'Vit',
+	location: 'Lundby',
+	bought: '2019-01-01',
+	service: {
+		nextHalf: '2019-09-30',
+		nextFull: '2021-12-30',
+		history: [
+			{
+				type: 'Halvservice',
+				date: '2020-01-10',
+				location: 'Toyota Göteborg',
+				comment: 'Went well, I guess!',
+			},
+			{
+				type: 'Fullservice',
+				date: '2019-01-10',
+				location: 'Toyota Göteborg',
+				comment: 'Went well, I guess!',
+			}
+		]
+	},
+	inspection: {
+		next: '2019-10-30',
+		history: [
+			{
+				date: '2020-01-10',
+				location: 'Toyota Göteborg',
+				comment: 'Went well, I guess!',
+			},
+		]
+	},
+	fuel: [
+		{ date: '19-07-25', year: '2019', month: '07', day: '25', cost: '309' },
+		{ date: '19-07-31', year: '2019', month: '07', day: '31', cost: '500' },
+		{ date: '19-08-14', year: '2019', month: '08', day: '14', cost: '470' },
+		{ date: '19-09-01', year: '2019', month: '09', day: '01', cost: '350' },
+		{ date: '19-09-15', year: '2019', month: '09', day: '15', cost: '674' },
+		{ date: '19-10-03', year: '2019', month: '10', day: '03', cost: '555' },
+	],
+	mileage: [
+		{ date: '19-07-25', year: '2019', month: '07', day: '25', cost: '4000' },
+		{ date: '19-09-01', year: '2019', month: '09', day: '01', cost: '6320' },
+		{ date: '19-10-03', year: '2019', month: '10', day: '03', cost: '6999' },
+	]
+}
+
 function Test(props) {
 	const [mileageAverageOpen, setMileageAverageOpen] = useState(false);
 	const [mileageOpen, setMileageOpen] = useState(false);
 	const [fuelCostOpen, setFuelCostOpen] = useState(false);
 	const [fuelCostMonthOpen, setFuelCostMonthOpen] = useState(false);
-	
+
 	const fuelDivs = fuelArray.map((item, index) => {
 		return (
 			<div key={'fuelDivs' + index} style={{ marginBottom: '10px' }}>
@@ -122,7 +186,51 @@ function Test(props) {
 				<br />
 			</div>
 		)
-	})
+	});
+
+	const wheelsHtml = { winterImages: [], winterText: [], summerImages: [], summerText: [] };
+	for (let i = 1; i < 5; i++) {
+		wheelsHtml.winterImages.push(
+			<Grid.Column width={3} key={'winwheimg' + i}>
+				{i <= carData.wheels.winter.amount ? <h1 className="display-3"><GiCarWheel /></h1> : <h1 className="display-3 text-danger"><ImCross /></h1>}
+			</Grid.Column>
+		);
+		wheelsHtml.winterText.push(
+			<Grid.Column width={3} key={'winwhetxt' + i}>
+				{i <= carData.wheels.winter.amount ? <h3>Allround <Icon name="snowflake" /></h3> : <></>}
+			</Grid.Column>
+		);
+		wheelsHtml.summerImages.push(
+			<Grid.Column width={3} key={'winwheimg' + i}>
+				{i <= carData.wheels.summer.amount ? <h1 className="display-3"><GiCarWheel /></h1> : <h1 className="display-3 text-danger"><ImCross /></h1>}
+			</Grid.Column>
+		);
+		wheelsHtml.summerText.push(
+			<Grid.Column width={3} key={'winwhetxt' + i}>
+				{i <= carData.wheels.summer.amount ? <h3>Sommar</h3> : <></>}
+			</Grid.Column>
+		);
+	}
+	if (carData.wheels.current === 'winter') {
+		wheelsHtml.winterImages.push(
+			<Grid.Column width={3} key={'winwheimgxtra'}>
+				<h1 className="display-3 text-success"><HiArrowLeft /></h1>
+			</Grid.Column>);
+		wheelsHtml.winterText.push(
+			<Grid.Column width={3} key={'winwheimgxtra'}>
+				<h3 className="text-success">Nuvarande</h3>
+			</Grid.Column>);
+	} else {
+		wheelsHtml.summerImages.push(
+			<Grid.Column width={3} key={'winwheimgxtra'}>
+				<h1 className="display-3 text-success"><HiArrowLeft /></h1>
+			</Grid.Column>);
+		wheelsHtml.summerText.push(
+			<Grid.Column width={3} key={'winwheimgxtra'}>
+				<h3 className="text-success">Nuvarande</h3>
+			</Grid.Column>);
+	}
+
 
 	return (
 		<div className="container-fluid center" style={{ width: '90%', marginLeft: '5%', marginRight: '5%', marginTop: '0px' }}>
@@ -135,6 +243,71 @@ function Test(props) {
 					<Dropdown defaultValue='2019' options={optionsYears} />
 				</div>*/}
 				<Grid verticalAlign='middle'>
+					<Grid.Row className="mb-5">
+						<Grid.Column width={8}>
+							<Grid verticalAlign='middle'>
+								<Grid.Row>
+									{wheelsHtml.winterImages}
+								</Grid.Row>
+								<Grid.Row>
+									{wheelsHtml.winterText}
+								</Grid.Row>
+								<Grid.Row>
+									{wheelsHtml.summerImages}
+								</Grid.Row>
+								<Grid.Row>
+									{wheelsHtml.summerText}
+								</Grid.Row>
+							</Grid>
+						</Grid.Column>
+						<Grid.Column width={8}>
+							<center>
+								<Grid className="h2" textAlign="left">
+									<Grid.Row>
+										<Grid.Column width={8}>
+											Försäkring / år
+										</Grid.Column>
+										<Grid.Column width={8}>
+											{carData.insuranceCost}
+										</Grid.Column>
+									</Grid.Row>
+									<Grid.Row>
+										<Grid.Column width={8}>
+											Köpt
+										</Grid.Column>
+										<Grid.Column width={8}>
+											{carData.bought}
+										</Grid.Column>
+									</Grid.Row>
+									<Grid.Row>
+										<Grid.Column width={8}>
+											Besiktning
+										</Grid.Column>
+										<Grid.Column width={8} className="text-danger">
+											{carData.inspection.next}
+										</Grid.Column>
+									</Grid.Row>
+									<Grid.Row>
+										<Grid.Column width={8}>
+											Service
+										</Grid.Column>
+										<Grid.Column width={8} className="text-danger">
+											{carData.service.nextHalf}
+										</Grid.Column>
+									</Grid.Row>
+									<Grid.Row>
+										<Grid.Column width={8}>
+											Fullservice
+										</Grid.Column>
+										<Grid.Column width={8} className={((new Date(carData.service.nextFull) - new Date(carData.service.nextFull)) / (1000 * 3600 * 24 * 30)) <= 2 ? 'text-danger' : ''}>
+											{carData.service.nextFull}
+											{(((new Date(carData.service.nextFull).getTime()) - (new Date(carData.service.nextFull)).getTime()) / (1000 * 3600 * 24 * 30)).toString()}
+										</Grid.Column>
+									</Grid.Row>
+								</Grid>
+							</center>
+						</Grid.Column>
+					</Grid.Row>
 					<Grid.Row>
 						<Grid.Column width={8}>
 							<h1>Miltal</h1>
